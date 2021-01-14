eba52e9be3098323faca848bfa9423b18eee7e41
Kaabocha <milky42744274@gmail.com>
2021-01-14 15:11:26 +0900

    モデルにカラムを追加

--- db/migrate/20210113150146_devise_create_users.rb
+++ db/migrate/20210113150146_devise_create_users.rb
@@ -6,6 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :name,               null: false
+      t.text   :a_word,             null: false
+      t.text   :title
 
       ## Recoverable
       t.string   :reset_password_token
