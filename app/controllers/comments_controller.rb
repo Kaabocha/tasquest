bb71f20690fcc52039ca85bbf57db86180470f77
Kaabocha <milky42744274@gmail.com>
2021-02-14 19:29:55 +0900

    viewの修正

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+  @comment = Comment.new(comment_params)
+  if @comment.save
+    redirect_to task_path(@comment.task)
+  else
+    @task = @comment.task
+    @comments = @task.comments
+    render "tasks/show"
+  end
+end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, task_id: params[:task_id])
+  end
+end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,57 @@
 class TasksController < ApplicationController
-
+  before_action :set_prototype, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+  
   def index
+    @tasks = Task.all.includes(:user).order("created_at DESC")
   end
-
+  
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(Task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @comment = Comment.new
+    @comments = @task.comments.includes(:user)
+  end
+  
+  def update
+    if @task.update(task_params)
+      redirect_to task_path
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    if @task.destroy
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def move_to_index
+    redirect_to root_path unless current_user == @task.user
+  end
 end
